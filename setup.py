5ce3bbe757ac636b2a09cba256cb376cbcf01a16
Alexander Southan <72788772+AlexanderSouthan@users.noreply.github.com>
2021-10-18 16:44:23 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,5 +4,6 @@ setup(
     name='pyPreprocessing',
     version='0.0.1',
     packages=find_packages(where='src'),
-    install_requires=['numpy', 'pandas', 'scipy', 'matplotlib', 'scikit-learn', 'tqdm']
+    install_requires=['numpy', 'pandas', 'scipy', 'matplotlib', 'scikit-learn', 'tqdm'],
+    dependency_links=['http://github.com/user/repo/tarball/master#egg=package-1.0']
 )
