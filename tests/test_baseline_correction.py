d9b9567ad3dc39579ab8840eb91b3096dcd601d6
Snijderfrey <peter.vandersnijderfrey@web.de>
2021-10-19 00:13:26 +0200

    Set up structure for testing

---
+++ tests/test_baseline_correction.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 16 19:59:07 2021
+
+@author: Alexander Southan
+"""
+
+import numpy as np
+import unittest
+
+from src.pyPreprocessing import baseline_correction
+
+
+class TestBaselineCorrection(unittest.TestCase):
+
+    def test_baseline_correction(self):
+
+        pass
