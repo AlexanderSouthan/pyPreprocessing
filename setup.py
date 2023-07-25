36b4d7a671f2a6cb4ca0a992ff9faf278e16a36c
Alexander Southan <72788772+AlexanderSouthan@users.noreply.github.com>
2023-07-25 12:57:35 +0200

    Increased version number to 0.0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pyPreprocessing',
-    version='0.0.1',
+    version='0.0.2',
     packages=find_packages(where='src'),
     install_requires=['numpy', 'pandas', 'scipy', 'matplotlib', 'scikit-learn', 'tqdm'],
     dependency_links=['http://github.com/user/repo/tarball/master#egg=package-1.0']
